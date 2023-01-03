8bf8fda38c98e35e508acd0b437875f1e281bd81
Stephanie Hill <stephni.hill@gmail.com>
2023-01-03 14:17:44 -0800

    new promise

---
+++ promise.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const myPromise = new Promise((resolveFunction, rejectFunction) => {
+  setTimeout(() => {
+    resolveFunction('Promise Kept');
+  }, 100);
+});
+
+myPromise.then((value) => {
+  console.log(value);
+  // expected output: 'Promise Kept'
+});
+
+console.log('Promise Object ---->', myPromise);
+// expected output: Promise { <pending> }
